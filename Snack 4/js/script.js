a8eb70188ff6aba092f683153fe61e7b5a4e125d
WalterCorsini <corsini.walter@gmail.com>
2024-04-16 12:30:56 +0200

    add snack 4

--- Snack 4/js/script.js
+++ Snack 4/js/script.js
@@ -15,5 +15,14 @@ const persone = [
         eta: "10",
     },
 ];
-
-const
+let personeMagg=[];
+let personeMin=[];
+persone.forEach((curElem) => {
+    if(curElem.eta >= 18){
+        personeMagg.push(curElem);
+    } else{
+        personeMin.push(curElem);
+    }
+});
+console.log("persone che possono guidare",personeMagg);
+console.log("persone che non possono guidare",personeMin);
